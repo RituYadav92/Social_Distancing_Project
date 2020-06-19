0db6e4146a0d93b5a57c92278ed22c3ecf34b83d
RituYadav92 <er.ritu92@gmail.com>
2020-06-19 02:54:25 +0200

    removed extra spacing

--- distance_detection/detection.py
+++ distance_detection/detection.py
@@ -4,43 +4,43 @@ import numpy as np
 import cv2
 
 def detect_people(frame, net, ln, personIdx=0):
-    (H, W) = frame.shape[:2]
-    results = []
+	(H, W) = frame.shape[:2]
+	results = []
 
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-        swapRB=True, crop=False)
-    net.setInput(blob)
-    layerOutputs = net.forward(ln)
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	layerOutputs = net.forward(ln)
 
-    boxes = []
-    centroids = []
-    confidences = []
+	boxes = []
+	centroids = []
+	confidences = []
 
-    # loop over each of the layer outputs
-    for output in layerOutputs:
-        # loop over each of the detections
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
+	for output in layerOutputs:
+		for detection in output:
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
 
-            if classID == personIdx and confidence > MIN_CONF:
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-                boxes.append([x, y, int(width), int(height)])
-                centroids.append((centerX, centerY))
+			if classID == personIdx and confidence > MIN_CONF:
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
 
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
 
-    # ensure at least one detection exists
-    if len(idxs) > 0:        
-        for i in idxs.flatten():            
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
+				boxes.append([x, y, int(width), int(height)])
+				centroids.append((centerX, centerY))
+				confidences.append(float(confidence))
 
-            r = (confidences[i], (x, y, x + w, y + h), centroids[i])
-            results.append(r)
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
 
-    return results
+	if len(idxs) > 0:
+		for i in idxs.flatten():
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+
+			r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+			results.append(r)
+
+	return results
